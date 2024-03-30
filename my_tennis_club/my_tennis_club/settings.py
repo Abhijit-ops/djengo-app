306eddc321f4486f2578aee3e7abb5c2cf8ac61c
AbhijitKalita <abhijitkalita1984@gmail.com>
2024-03-30 18:44:53 +0530

    templates

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-))+0lcz-w0a)qc6r#k1+*qm32xe82eqwe7-ct(s3zusl1nn8i&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False #True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
